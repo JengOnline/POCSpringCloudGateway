94a0c4ddec461b3d4cd570ea86162d676df25a51
Sophon Jumpapang <67536359+JengOnline@users.noreply.github.com>
2023-02-26 16:40:31 +0700

    Add files via upload

---
+++ second/src/main/java/second/microservice/second/SecondApplication.java
@@ -0,0 +1,13 @@
+package second.microservice.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondApplication.class, args);
+	}
+
+}
---
+++ second/src/test/java/second/microservice/second/SecondApplicationTests.java
@@ -0,0 +1,13 @@
+package second.microservice.second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
